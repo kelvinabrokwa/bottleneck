14c902d4e812ac895a48b717633f73688f40ca98
kelvinabrokwa <kelvinabrokwa@gmail.com>
2016-11-23 17:51:12 -0500

    first

---
+++ README.md
@@ -0,0 +1,22 @@
+# bottleneck
+
+C++ code for calculating bottleneck distance with mex code for Matlab interop
+
+Written by Miro Kramer (2013), modified by Kelvin Abrokwa (2016)
+
+### Compiling
+
+Depends on `g++` compiler
+
+```sh
+make
+```
+
+### Testing
+
+run `test_bottleneck.sh`
+
+```sh
+./test_bottleneck.sh
+```
+
---
+++ bottleneckDist.cpp
@@ -0,0 +1,47 @@
+//============================================================================
+// Name        : bottleneckDist.cpp
+// Author      : Jesse Berwald (modified Miro's code)
+// Version     :
+// Copyright   : Your copyright notice
+// Description :
+//============================================================================
+
+#include <iostream>
+#include <fstream>
+#include "CBottleneckDistance.hpp"
+
+using namespace std;
+
+
+int main(int argc, char* argv[])
+{
+    // must pass in two args, one for each persdia file, so #argc = 1 + 2
+    // may also want to pass in max_gen later
+    if (argc < 3 || argc > 4) {
+        cout
+            << endl
+            << "Must pass in filename1 and filename2"
+            << endl
+            << "to find the distance between diagrams."
+            << endl;
+        return -1;
+    }
+
+    // file names
+    char* file1 = argv[1];
+    char* file2 = argv[2];
+
+    CBottleneckDistance d;
+
+    int n_files = 2;
+    // int B = 1;
+
+    double max_gen = 1;
+
+    // compute the distance between persistence diagrams
+    double distance = d.Distance(file1, file2, max_gen);
+
+    cout << distance << endl;
+
+    return 0;
+}
